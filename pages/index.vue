936e5a88c50a5a7d0a21f24aa92094c45134d775
kkangda7 <hoon.kang@anex.co.jp>
2022-03-23 17:30:42 +0900

    first

---
+++ pages/index.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <div>
+      <input type="text" placeholder="入力してください" />
+    </div>
+    <div>
+      <input type="text" />
+    </div>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
