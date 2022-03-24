dc31653bab321ac2d5e9b8c44ebe9ebc78f51344
kkangda7 <hoon.kang@anex.co.jp>
2022-03-24 11:49:55 +0900

    header

---
+++ components/Header.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="header">
+    <div class="header__logo">
+      <img src="@/assets/images/logo.svg" />
+    </div>
+    <div class="header__alert">
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
