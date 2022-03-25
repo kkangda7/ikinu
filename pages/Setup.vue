d453de11513ac2c47833efd527e03dde7126f81e
kkangda7 <hoon.kang@anex.co.jp>
2022-03-25 13:49:01 +0900

    setup

--- pages/Setup.vue
+++ pages/Setup.vue
@@ -1,9 +1,34 @@
 <template>
   <div>
     <Header></Header>
-    setup
-    <Footer></Footer>
+    <div class="setup">
+    <div class="setup__body">
+      <dl class="setup__body__list">
+        <dt>各種設定</dt>
+        <dd>
+          <button class="btn btn--medium btn--flat_orange">プロフィール編集</button>
+        </dd>
+        <dd>
+          <button class="btn btn--medium btn--flat_orange">メールアドレス編集</button>
+        </dd>
+        <dd>
+          <button class="btn btn--medium btn--flat_orange">パスワード編集</button>
+        </dd>
+        <dd>
+          <button class="btn btn--medium btn--flat_orange">お問い合わせ</button>
+        </dd>
+        <dd>
+          <button class="btn btn--medium btn--flat_orange">利用契約</button>
+        </dd>
+        <dd>
+          <button class="btn btn--medium btn--flat_orange">プライバシーポリシー</button>
+        </dd>
+      </dl>
+    </div>
   </div>
+  <Footer></Footer>
+</div>
+
 </template>
 
 <script>
